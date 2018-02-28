4a308e0a51f7b7193cdb955262e3fc6cd5741e32
parksm13 <parksm13@students.ecu.edu>
2018-02-28 17:48:10 -0500

    Added 'Add Print' button

--- src/gui/PPSFrame.java
+++ src/gui/PPSFrame.java
@@ -1,12 +1,29 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PPSFrame extends JFrame {
     private PPSFrame() {
         setSize(800, 600);
         setTitle("PPS");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+        panel.setLayout(null);
+
+        JButton addPrint = new JButton("Add Print");
+        addPrint.setBounds(150, 60, 80, 30);
+        addPrint.addActionListener(new ActionListener() {
+                                       @Override
+                                       public void actionPerformed(ActionEvent e) {
+
+                                       }
+        });
+
+        panel.add(addPrint);
     }
 
     public static void main (String[] args) {
