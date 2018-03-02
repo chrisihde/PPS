e97cb0fb322d80e3ecf89da3a50ec12f33ae536f
parksm13 <parksm13@students.ecu.edu>
2018-03-02 12:18:07 -0500

    Refactored to simplify adding new buttons

---
+++ src/Enums/Button.java
@@ -0,0 +1,16 @@
+package Enums;
+
+public enum Button {
+    ADD_PRINT;
+
+    private int value;
+
+    static {
+        ADD_PRINT.value = 0;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
--- src/gui/PPSFrame.java
+++ src/gui/PPSFrame.java
@@ -3,31 +3,49 @@ package gui;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.ArrayList;
+
+import DAL.Print;
+import Enums.Button;
 
 public class PPSFrame extends JFrame {
+
+    private static JPanel panel;
+    private static List<JButton> buttons = new ArrayList<>();
+    private static List<Print> prints = new ArrayList<>();
+
     private PPSFrame() {
         setSize(800, 600);
         setTitle("PPS");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         getContentPane().add(panel);
         panel.setLayout(null);
-
-        JButton addPrint = new JButton("Add Print");
-        addPrint.setBounds(150, 60, 80, 30);
-        addPrint.addActionListener(new ActionListener() {
-                                       @Override
-                                       public void actionPerformed(ActionEvent e) {
-
-                                       }
-        });
-
-        panel.add(addPrint);
     }
 
-    public static void main (String[] args) {
+    private static void createButtons() {
+        JButton addPrint = new JButton("Add Print");
+        addPrint.setBounds(150, 60, 100, 30);
+        addPrint.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        buttons.add(Button.ADD_PRINT.getValue(), addPrint);
+    }
+
+    private static void addButtonsToPanel() {
+        panel.add(buttons.get(Button.ADD_PRINT.getValue()));
+    }
+
+    public static void main(String[] args) {
         PPSFrame frame = new PPSFrame();
+        PPSFrame.createButtons();
+        PPSFrame.addButtonsToPanel();
         frame.setVisible(true);
     }
 }
