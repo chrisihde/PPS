e1f189afdffaa8fc1b6e6cc84cc24ec779a678af
parksm13 <parksm13@students.ecu.edu>
2018-03-02 14:58:37 -0500

    Added more subpanels within print settings panel to improve layout

--- src/gui/PPSFrame.java
+++ src/gui/PPSFrame.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -14,11 +15,21 @@ import Enums.PrintSize;
 
 public class PPSFrame extends JFrame {
 
+    // main panels
     private static JPanel containerPanel;
     private static JPanel printSettingsPanel;
     private static JPanel printInfoPanel;
+
+    // subpanels
+    private static JPanel printSettingsLabelPanel;
+    private static JPanel printSizePanel;
+    private static JPanel addPrintPanel;
+
+    //widgets
     private static List<JButton> buttons = new ArrayList<>();
     private static List<JComboBox> dropdowns = new ArrayList<>();
+
+    // data
     private static Order order = new Order();
 
     private PPSFrame() {
@@ -26,15 +37,28 @@ public class PPSFrame extends JFrame {
         setTitle("PPS");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        // the main window
         containerPanel = new JPanel();
         containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.X_AXIS));
 
+        // the left and right halves of the window
         printSettingsPanel = new JPanel();
         printInfoPanel = new JPanel();
 
+        // each row in the left window
+        printSettingsLabelPanel = new JPanel();
+        printSizePanel = new JPanel();
+        addPrintPanel = new JPanel();
+        printSettingsPanel.setLayout(new GridLayout(4,2));
+        printSettingsPanel.add(printSettingsLabelPanel);
+        printSettingsPanel.add(printSizePanel);
+        printSettingsPanel.add(addPrintPanel);
+
+        // add left and right sides to main window
         containerPanel.add(printSettingsPanel);
         containerPanel.add(printInfoPanel);
 
+        // add main panel to window
         getContentPane().add(containerPanel);
     }
 
@@ -59,7 +83,7 @@ public class PPSFrame extends JFrame {
     }
 
     private static void addButtonsToPanel() {
-        printSettingsPanel.add(buttons.get(Button.ADD_PRINT.getValue()));
+        addPrintPanel.add(buttons.get(Button.ADD_PRINT.getValue()));
     }
 
     private static void createDropdowns() {
@@ -72,19 +96,26 @@ public class PPSFrame extends JFrame {
     }
 
     private static void addDropdownsToPanel() {
-        printSettingsPanel.add(dropdowns.get(Dropdown.PRINT_SIZE.getIndex()));
+        // print size
+        printSizePanel.add(new JLabel("Print Size: "));
+        printSizePanel.add(dropdowns.get(Dropdown.PRINT_SIZE.getIndex()));
     }
 
-    private static void setupInfoPanel() {
-        printInfoPanel.add(new JLabel("test"));
-    }
-
-    public static void main(String[] args) {
-        PPSFrame frame = new PPSFrame();
+    private static void setupSettingsPanel() {
+        printSettingsLabelPanel.add(new JLabel("Print Settings: "));
         PPSFrame.createButtons();
         PPSFrame.addButtonsToPanel();
         PPSFrame.createDropdowns();
         PPSFrame.addDropdownsToPanel();
+    }
+
+    private static void setupInfoPanel() {
+        printInfoPanel.add(new JLabel("Print Info:"));
+    }
+
+    public static void main(String[] args) {
+        PPSFrame frame = new PPSFrame();
+        PPSFrame.setupSettingsPanel();
         PPSFrame.setupInfoPanel();
         frame.setVisible(true);
     }
