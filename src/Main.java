c4f6558f672041fa7d2ec93a2806f65617e392bf
parksm13 <parksm13@students.ecu.edu>
2018-03-06 14:21:53 -0500

    Added framework for MVC refactor

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import Control.PPSControl;
+import Model.PPSModel;
+import View.PPSView;
 import gui.PPSFrame;
 
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        PPSFrame frame = new PPSFrame();
-        PPSFrame.setupSettingsPanel();
-        PPSFrame.setupInfoPanel();
-        frame.setVisible(true);
-    }
+
+        private static void createAndShowGui() {
+            PPSView view = new PPSView("PPS");
+            PPSModel model = new PPSModel();
+            PPSControl control = new PPSControl(view, model);
+
+            control.showView(true);
+        }
+
+        public static void main(String[] args) {
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    createAndShowGui();
+                }
+            });
+        }
 }
